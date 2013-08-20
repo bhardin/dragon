63deff6e62a406b751df9a120b760cbc7acee444
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-08-19 20:46:55 -0700

    Need this behavior to function.

--- spec/source/parser/container_spec.rb
+++ spec/source/parser/container_spec.rb
@@ -3,6 +3,7 @@ require "spec_helper"
 describe Dragon::Parser::Container do
   let(:parser) do
     class ExampleParser < Parslet::Parser
+      include Dragon::Parser::Expression
       include Dragon::Parser::Container
     end.new
   end
